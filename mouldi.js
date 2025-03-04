571a8982466e27e98db7633148f292ea7e87b691
bannouri mouldi <bannourimouldi22@gmail.com>
2025-03-04 10:08:34 +0100

    project

--- mouldi.js
+++ mouldi.js
@@ -1,22 +1,16 @@
 $(document).ready(function() {
-   
     $('#formulaire-inscription').on('submit', function(event) {
         event.preventDefault(); 
-
         var nom = $('#nom').val();
         var email = $('#email').val();
-
         localStorage.setItem('nom_utilisateur', nom);
         localStorage.setItem('email_utilisateur', email);
-
         alert('Merci ' + nom + '! Votre inscription a été enregistrée avec l\'email : ' + email);
-
         window.location.href = "mo.html";
     });
-
     
     $('#btn-inscription').on('click', function(event) {
-        event.preventDefault();
+        event.preventDefault(); 
         $('#formulaire-inscription').submit(); 
     });
 });
